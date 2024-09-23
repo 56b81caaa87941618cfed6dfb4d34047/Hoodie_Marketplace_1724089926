1b1b1e4fd4de97b9f46ff9d0c39a0e6f322700b1
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-09-23 18:11:38 +0000

    Merge code from frontend

--- components/page_details_cards_component_1724089929.js
+++ components/page_details_cards_component_1724089929.js
@@ -3,23 +3,7 @@
 */
 Vue.component("page_details_cards_component_1724089929", {
     template: `
-    <section id="page-details-cards-component" class="py-8 bg-white">
-        <h2 id="details-title" class="text-lg font-semibold mb-5 pl-4">Details</h2>
-        <ul id="details-list" class="grid gap-4 min-[480px]:grid-cols-3 text-sm">
-            <li id="project-length" class="px-5 py-4 rounded-lg bg-gradient-to-tr from-slate-950 to-slate-800 dark:from-slate-800/80 dark:to-slate-900">
-                <div class="font-medium text-slate-200 text-slate-400">Unique Designs</div>
-                <div class="text-slate-400">Explore our vast collection of one-of-a-kind hoodie designs, created by talented artists from around the world.</div>
-            </li>
-            <li id="start-date" class="px-5 py-4 rounded-lg bg-gradient-to-tr from-slate-950 to-slate-800 dark:from-slate-800/80 dark:to-slate-900">
-                <div class="font-medium" id="start-date-des-1 text-slate-200 text-slate-400">Quality Comfort</div>
-                <time class="text-slate-400" id="start-time-des-1">Experience unparalleled comfort with our premium materials and expert craftsmanship, ensuring your hoodie feels as good as it looks.</time>
-            </li>
-            <li id="end-date" class="px-5 py-4 rounded-lg bg-gradient-to-tr from-slate-950 to-slate-800 dark:from-slate-800/80 dark:to-slate-900">
-                <div class="font-medium" id="end-date-des-1 text-slate-200 text-slate-400">Easy Shopping</div>
-                <time class="text-slate-400" id="end-time-des-1">Browse, customize, and purchase your perfect hoodie with our user-friendly interface and secure checkout process.</time>
-            </li>
-        </ul>
-    </section>
+    <section id="page-details-cards-component" class="py-8 bg-white" style=""><h2 id="details-title" class="text-lg font-semibold mb-5 pl-4" style="" data-mf-new="true">DETAILS</h2> <ul id="details-list" class="grid gap-4 min-[480px]:grid-cols-3 text-sm" style=""><li id="project-length" class="px-5 py-4 rounded-lg bg-gradient-to-tr from-slate-950 to-slate-800 dark:from-slate-800/80 dark:to-slate-900" style=""><div class="font-medium text-slate-200 text-slate-400" style="" data-mf-new="true">Unique Designs</div> <div class="text-slate-400" style="" data-mf-new="true">Explore our vast collection of one-of-a-kind hoodie designs, created by talented artists from around the world.</div></li> <li id="start-date" class="px-5 py-4 rounded-lg bg-gradient-to-tr from-slate-950 to-slate-800 dark:from-slate-800/80 dark:to-slate-900" style=""><div id="start-date-des-1 text-slate-200 text-slate-400" class="font-medium" style="">Quality Comfort</div> <time id="start-time-des-1" class="text-slate-400" style="" data-mf-new="true">Experience unparalleled comfort with our premium materials and expert craftsmanship, ensuring your hoodie feels as good as it looks.</time></li> <li id="end-date" class="px-5 py-4 rounded-lg bg-gradient-to-tr from-slate-950 to-slate-800 dark:from-slate-800/80 dark:to-slate-900"><div id="end-date-des-1 text-slate-200 text-slate-400" class="font-medium">Easy Shopping</div> <time id="end-time-des-1" class="text-slate-400">Browse, customize, and purchase your perfect hoodie with our user-friendly interface and secure checkout process.</time></li></ul></section>
     `,
     data() {
         return {
